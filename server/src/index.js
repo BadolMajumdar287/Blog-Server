235232782260ea6659beec0d012aa6e12d3f30fa
BadolMajumdar287 <badolmajumdar2006@gmail.com>
2025-07-20 14:04:17 +0530

    add cors

--- server/src/index.js
+++ server/src/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import {config} from  "dotenv";
 import cookieparser from "cookie-parser";
 import { AdminRouter } from "./router/admin.router.js";
@@ -7,7 +8,7 @@ import { Databaseconfig } from "./config/mongoose.config.js";
 
 
 config();
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
@@ -15,6 +16,15 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieparser());
+
+
+
+
+app.use(cors({
+    origin: ["http://localhost:3000", "http://192.168.100.191:3000"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true,
+}));
 
 
 
